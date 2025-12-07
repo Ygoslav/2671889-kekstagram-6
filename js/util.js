47bc184103a16a6e43415128468c2ea93bcfae76
Ygoslav <gorlovxxx@gmail.com>
2025-12-08 03:51:18 +0500

    Исправляет функцию получения случайного целого числа и добавляет новые служебные функции

--- js/util.js
+++ js/util.js
@@ -1,9 +1,15 @@
 const getRandomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min) + min;
-
+  Math.floor(Math.random() * (max - min + 1)) + min;
 
 const getRandomArrayElement = (array) =>
   array[getRandomInt(0, array.length - 1)];
 
+const isEscapeKeyPressed = (evt) => evt.key === 'Escape';
 
-export {getRandomInt, getRandomArrayElement};
+const toggleClass = (element, className) => {
+  if (element) {
+    element.classList.toggle(className);
+  }
+};
+
+export { getRandomInt, getRandomArrayElement, isEscapeKeyPressed, toggleClass };
