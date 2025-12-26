736a83b76fe7e123cb6eccb36cb7ccc891e01923
Ygoslav <gorlovxxx@gmail.com>
2025-12-26 23:11:48 +0500

    Добавляет функцию для устранения дребезга

--- js/util.js
+++ js/util.js
@@ -14,10 +14,19 @@ const toggleClass = (element, className) => {
 
 const checkStringLength = (string, length) => string.length <= length;
 
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
 export {
   getRandomInt,
   getRandomArrayElement,
   isEscapeKeyPressed,
   toggleClass,
   checkStringLength,
+  debounce,
 };
